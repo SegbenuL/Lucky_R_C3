ef0248bdb594b7c4af44eb7b433edc359747891e
SegbenuL <segbenu.lucky@azubiafrica.org>
2023-08-28 11:38:15 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <><h1>Hello world</h1>
+    <p>I am SEGBENU LUCKY DAMILOLA, this is my first React application</p>
+    </>
+  );
+}
+
+export default App;
